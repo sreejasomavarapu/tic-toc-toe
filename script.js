5b48fe0aa49a401124ecf9fa389340f75666ed09
sreejasomavarapu <sreejasomavarapu29@gmail.com>
2022-05-25 21:06:51 +0530

    Final Website using html, css and JavaScript

---
+++ script.js
@@ -0,0 +1,74 @@
+console.log("Welcome To Tic Toc Toe")
+// https://www.w3schools.com/js/js_let.asp
+let music = new Audio("bgmusic.mp3")
+let Audioturn = new Audio("ting.mp3")
+let turn = "X"
+let isgameover=false
+
+// Function to change the turn
+const changeTurn=()=>{
+    return turn=="X"?"0":"X"
+}
+
+// Funtion to check for a Win
+const checkWin=()=>{
+    let boxtext = document.getElementsByClassName("boxtext");
+
+    let wins=[
+        [0,1,2],
+        [3,4,5],
+        [6,7,8],
+        [0,3,6],
+        [1,4,7],
+        [2,5,8],
+        [0,4,8],
+        [2,4,6]    
+    ]
+    wins.forEach(e=>{
+        if ((boxtext[e[0]].innerText==boxtext[e[1]].innerText)&&(boxtext[e[0]].innerText==boxtext[e[2]].innerText)&&(boxtext[e[0]].innerText!=="")){
+            document.querySelector('.info').innerText = boxtext[e[0]].innerText+" Won"
+            isgameover=true
+            document.querySelector('.imgbox').getElementsByTagName("img")[0].style.width="200px"
+
+        }    })
+
+
+}
+
+
+//Game Logic
+//If you want to access any element in an HTML page, you always start with accessing the document object.
+let boxes=document.getElementsByClassName("box");
+Array.from(boxes).forEach(element=>{
+    let boxtext = element.querySelector('.boxtext');
+    element.addEventListener('click',()=>{
+        if (boxtext.innerText===''){
+            boxtext.innerText=turn;
+            turn=changeTurn(); 
+            Audioturn.play();
+            checkWin();
+            if (!isgameover){
+            document.getElementsByClassName("info")[0].innerText ="Turn for "+ turn;
+            }
+        }
+    })
+})
+// Reset button
+reset.addEventListener('click',()=>{
+    let boxtext = document.querySelectorAll('.boxtext');
+    Array.from(boxtext).forEach(element=>{
+        element.innerText=""
+    })
+    turn="X";
+    isgameover=false
+    document.getElementsByClassName("info")[0].innerText ="Turn for "+ turn;
+    document.querySelector('.imgbox').getElementsByTagName("img")[0].style.width="0px"
+
+
+
+})
+
+
+
+
+
